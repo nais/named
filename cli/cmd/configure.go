ab88eea49e86c2f294f4a0f1e4c7c167b4a5f0a1
Muni10 <sten.ivar.rokke@nav.no>
2018-02-19 13:10:39 +0100

    Bugfix for context root in cli

--- cli/cmd/configure.go
+++ cli/cmd/configure.go
@@ -83,7 +83,7 @@ var configurationCmd = &cobra.Command{
 		}
 
 		if api.ZoneFss == zone {
-			if value, err := cmd.Flags().GetStringArray("contexts"); err != nil && len(value) > 0 {
+			if value, err := cmd.Flags().GetStringArray("contextroots"); err != nil && len(value) > 0 {
 				configurationRequest.ContextRoots = value
 			}
 		}
@@ -135,7 +135,7 @@ func init() {
 	configurationCmd.Flags().StringP("version", "v", "", "version you want to configure for")
 	configurationCmd.Flags().StringP("cluster", "c", "", "the cluster you want to deploy to")
 	configurationCmd.Flags().StringP("environment", "e", "", "environment you want to use")
-	configurationCmd.Flags().StringP("contextroots", "r", "", "the contexts to configure in ISSO")
+	configurationCmd.Flags().StringP("contextroots", "r", "", "the context roots to configure in ISSO")
 	configurationCmd.Flags().StringP("username", "u", "", "the username")
 	configurationCmd.Flags().StringP("password", "p", "", "the password")
 	configurationCmd.Flags().Bool("wait", false, "whether to wait until the deploy has succeeded (or failed)")
